7320f87ae5c2cfcc5d7b476e19189474c217b5c2
Emil <emmehdiyev@yahoo.com>
2024-10-25 22:24:14 +0400

    first commit

--- src/main/java/az/edu/turing/Main.java
+++ src/main/java/az/edu/turing/Main.java
@@ -2,7 +2,9 @@ package az.edu.turing;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello World");
+        System.out.print("Hello, World");
+        System.out.println("Hello, World");
+        System.out.printf("Hello,%s", "World");
+        System.out.printf("Hello, World: %.2f\n", 50.0);
     }
 }
