f85e0e07e5a0b628ea7ec38d1f4070be7c126ac7
Emil <emmehdiyev@yahoo.com>
2024-10-26 16:11:03 +0400

    first commit

--- src/main/java/az/edu/turing/Main.java
+++ src/main/java/az/edu/turing/Main.java
@@ -1,10 +1,24 @@
 package az.edu.turing;
 public class Main {
-
     public static void main(String[] args) {
-        System.out.print("Hello, World");
-        System.out.println("Hello, World");
-        System.out.printf("Hello,%s", "World");
-        System.out.printf("Hello, World: %.2f\n", 50.0);
+        byte byteValue = 20;
+        short shortValue = 10000;
+        int intValue =400000;
+        long longValue = 70000000000L;
+        float floatValue = 3.14f;
+        double doubleValue = 3.14159;
+        char charValue = 'R';
+        boolean booleanValue = true;
+
+
+        System.out.println("Using println:");
+        System.out.println("short: " + shortValue);
+        System.out.println("int: " + intValue);
+        System.out.println("long: " + longValue);
+        System.out.printf("byte: %d\n", byteValue);;
+        System.out.printf("float: %.2f\n", floatValue);
+        System.out.printf("double: %.5f\n", doubleValue); //
+        System.out.printf("char: %c\n", charValue);
+        System.out.printf("boolean: %b\n", booleanValue);
     }
 }
